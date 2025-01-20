d49f383e21f47c62208e01a900170fdfd11850c0
indoxer <indoxer.mk@gmail.com>
2025-01-20 16:07:25 +0100

    Reduce maximum number of batched tokens in model configuration

--- constants.py
+++ constants.py
@@ -11,7 +11,7 @@ MODEL_CONFIG = {
     "gpu_memory_utilization": 0.95,
     "enable_chunked_prefill": True,
     "max_num_seqs": 16,
-    "max_num_batched_tokens": 4096,
+    "max_num_batched_tokens": 2048,
     "max_model_len": 16384,
 }
 
